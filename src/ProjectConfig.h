928702248e77f999e1fa13d38d1e63dd2eebb5ae
EdWard <ezeward4@gmail.com>
2015-12-27 22:39:23 -0500

    Removes src/ProjectConfig.h from tracked files and ignores

--- src/ProjectConfig.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROJECT_CONFIG_H_
-#define PROJECT_CONFIG_H_
-
-/* Contains project information from cmake */
-#define VERSION_MAJOR 0
-#define VERSION_MINOR 1
-#define GIT_REV "b80ed4c61dfef5e0ba045de40067b8173d2a6b0c"
-
-#endif
