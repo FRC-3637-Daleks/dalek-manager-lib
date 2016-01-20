d1dd570c472c12d419a199b6ca83939af9b83b73
EdWard <ezeward4@gmail.com>
2016-01-19 19:52:53 -0500

    Config: Adds TreeNode interface

---
+++ src/Config/TreeNode.cpp
@@ -0,0 +1,89 @@
+/*
+    Copyright (C) 2016, EdWard <dman>
+    ---
+    
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA  02110-1301, USA.
+
+ */
+
+// Project Includes
+#include "TreeNode.h"
+
+// Boost Includes
+#include <boost/range/any_range.hpp>
+
+// STD Includes
+#include <stdexcept>
+
+namespace dman
+{
+
+bool TreeNode::LoadConfig(json config)
+{
+	bool ret = false;
+
+	auto range = GetRange();
+	for(auto i : range)
+	{
+		try
+		{
+			// Attempts to pass to object the elment at the same key
+			ret |= i.second.LoadConfig(config.at(i.first));
+		}
+		catch (std::out_of_range& e)
+		{
+			// Signals failure
+			ret = true;
+		}
+	}
+
+	return ret;
+}
+
+json TreeNode::GetConfig() const
+{
+	json ret(json::value_t::object);
+
+	auto range = GetRange();
+	for(auto i : range)
+	{
+		ret[i.first] = i.second.GetConfig();
+	}
+
+	return ret;
+}
+
+json TreeNode::GetSchema() const
+{
+	json ret(json::value_t::object);
+
+	ret["type"] = "object";
+	ret["additionalProperties"] = false;
+
+	auto& required = (ret["required"] = json::array());
+	auto& properties = (ret["properties"] = json::object());
+
+	auto range = GetRange();
+	for(auto i : range)
+	{
+		required.push_back(i.first);
+		properties[i.first] = i.second.GetSchema();
+	}
+
+	return ret;
+}
+
+}  // namespace dman
---
+++ src/Config/TreeNode.h
@@ -0,0 +1,82 @@
+/*
+    Copyright (C) 2016 EdWard <ezeward4@gmail.com>
+    ---
+    
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA  02110-1301, USA.
+
+ */
+
+#ifndef SRC_CONFIG_TREENODE_H_
+#define SRC_CONFIG_TREENODE_H_
+
+// Project Includes
+#include "Node.h"
+
+// Boost Includes
+#include <boost/range/any_range.hpp>
+
+// STD Includes
+#include <string>
+#include <utility>
+
+namespace dman
+{
+
+class TreeNode: public Node
+{
+public:
+	/** Looks up json object at the key for each item in \c GetRange() \\
+	 * and passes into that item's LoadConfig
+	 * @pre \code{.cpp} config.is_object() == true \endcode
+	 * @return Returns true if any of the sub objects returned true, \\
+	 * or there wasn't a member in config at a required key
+	 */
+	bool LoadConfig(json config) override;
+
+	/** Returns json object formed with the json value returned by each item \\
+	 * in \c GetRange()
+	 * @return Returns a json object
+	 */
+	json GetConfig() const override;
+
+	/** Returns json object representing a schema with a required property \\
+	 * for each item in \c GetRange() formed from the schema that item returns
+	 * @return Returns a json object representing the schema
+	 */
+	json GetSchema() const override;
+
+protected:
+	using Key_t = std::string;
+	using Mapped_t = Node&;
+	using MapElement_t = std::pair<const Key_t, Mapped_t>;
+	using Range_t = boost::any_range<MapElement_t,
+									 boost::single_pass_traversal_tag,
+									 MapElement_t,
+									 std::ptrdiff_t>;
+
+protected:
+	/** Returns range of any forward iterator implementation.
+	 */
+	virtual Range_t GetRange() = 0;
+	const Range_t GetRange() const
+	{
+		return const_cast<TreeNode*>(this)->GetRange();
+	}
+};
+
+}  // namespace dman
+
+#endif  // SRC_CONFIG_TREENODE_H_
