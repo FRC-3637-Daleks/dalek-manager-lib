2aa8e1c8f68afead51b9385771ea8f08141de3ce
EdWard <ezeward4@gmail.com>
2016-02-27 01:25:15 -0500

    Utility: Made ValueStore::Value default constructible

--- src/Utility/ValueStore.h
+++ src/Utility/ValueStore.h
@@ -63,6 +63,7 @@ private:
 	struct ValueData
 	{
 		using Key_t = std::string;
+		ValueData() = default;
 		ValueData(Key_t key,
 				  typename Value<T>::Mapped_t value_ref,
 				  typename Value<T>::Setter_t *setter = nullptr,
@@ -98,6 +99,7 @@ public:
 		using Reference_t = Reference<T>;
 
 	public:
+		Value() = default;
 		explicit Value(ValueData<T> vd): data_(std::move(vd)) {}
 		Value(const Value&) = default;
 		Value(Value&&) = default;
